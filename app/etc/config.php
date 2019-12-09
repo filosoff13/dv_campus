db99995744bff235b38e0bdc4866aae795fcd2da
Vitalii Vitkovskyi <filosoff13@gmail.com>
2019-12-09 21:00:11 +0200

    hw#11: add file structure & layouts

--- app/etc/config.php
+++ app/etc/config.php
@@ -245,7 +245,6 @@ return [
         'MSP_TwoFactorAuth' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
-        'Vitaliiv_LayoutAdd' => 1,
         'Vitaliiv_LayoutDebug' => 1,
         'Yotpo_Yotpo' => 1
     ]
